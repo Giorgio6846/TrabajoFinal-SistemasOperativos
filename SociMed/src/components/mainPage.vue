abcd2669eb8f26d9ac74351a03beccd6baed0416
Mathias <mathiashv2014@gmail.com>
2024-06-14 20:01:16 -0500

    3 columns basic style

--- SociMed/src/components/mainPage.vue
+++ SociMed/src/components/mainPage.vue
@@ -1,38 +1,39 @@
 <template>
-  <div>
-    <ul>
-      <li>
-        <img
-          src="../assets/sociMedIMG.png"
-          alt="Image of the company SociMed"
-        />
-      </li>
-      <li>Inicio</li>
-      <li>Buscar</li>
-      <li>Perfil</li>
-    </ul>
-  </div>
-  <div>
-    <div class="search">
-      <input type="text" placeholder="¡¿Qué está pasando?!" />
+  <div class="mainPage">
+    <div class="firstColumn">
+      <ul>
+        <li>
+          <img
+            class="logoSociMed"
+            src="../assets/sociMedIMG.png"
+            alt="Image of the company SociMed"
+          />
+        </li>
+        <li>Inicio</li>
+        <li>Buscar</li>
+        <li>Perfil</li>
+      </ul>
     </div>
-  </div>
-  <div>
-    <div>
-      <h2>Tendencias</h2>
-      <div class="tendencias"></div>
+    <div class="secondColumn">
+      <div class="search">
+        <input type="text" placeholder="¡¿Qué está pasando?!" />
+      </div>
+    </div>
+    <div class="thirdColumn">
+      <div>
+        <h2>Tendencias</h2>
+        <div class="tendencias"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import "../styles/mainPage.css";
+
 export default {
   name: "mainPage",
 };
 </script>
 
-<style scoped>
-li {
-  list-style-type: none;
-}
-</style>
+<style></style>
