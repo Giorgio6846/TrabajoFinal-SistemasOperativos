96cab7aabc260e5dec01e7388be33a05778b2217
Drewks <u202211499@upc.edu.pe>
2024-06-14 22:51:53 -0500

    login and register

--- SociMed/src/router.js
+++ SociMed/src/router.js
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import mainPage from "./components/mainPage.vue";
 import loginPage from "./components/loginPage.vue";
+import registerPage from "./components/registerPage.vue";
 
 const routes = [
   { path: "/", component: mainPage },
   { path: "/login", component: loginPage },
+  { path: "/register", component: registerPage },
 ];
 
 const router = createRouter({
