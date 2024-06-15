2781b86ff9e2fe57f4c425177660fd041030fa7e
Mathias <mathiashv2014@gmail.com>
2024-06-14 19:41:23 -0500

    primera version mainPage

--- SociMed/src/components/mainPage.vue
+++ SociMed/src/components/mainPage.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>mainPage</div>
+  <div>
+    <ul>
+      <li>
+        <img
+          src="../assets/sociMedIMG.png"
+          alt="Image of the company SociMed"
+        />
+      </li>
+      <li>Inicio</li>
+      <li>Buscar</li>
+      <li>Perfil</li>
+    </ul>
+  </div>
+  <div>
+    <div class="search">
+      <input type="text" placeholder="¡¿Qué está pasando?!" />
+    </div>
+  </div>
+  <div>
+    <div>
+      <h2>Tendencias</h2>
+      <div class="tendencias"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +31,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+li {
+  list-style-type: none;
+}
+</style>
